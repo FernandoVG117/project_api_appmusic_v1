67fd82237c17daffa6562d33d0b1179f8a2edc25
Fernando <ing.fernandovg@gmail.com>
2024-08-02 14:39:07 -0600

    create Song model with its controller an router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const routerArtist = require('./artist.router');
 const routerAlbum = require('./album.router');
 const routerGenre = require('./genre.router');
+const routerSong = require('./song.router');
 const router = express.Router();
 
 router.use('/artists', routerArtist);
 router.use('/albums', routerAlbum);
 router.use('/genres', routerGenre);
+router.use('/songs', routerSong);
 
 
 module.exports = router;
