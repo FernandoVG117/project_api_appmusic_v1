e59e88a1e70c583ab5133fd4bceb18a52bdbd593
Fernando <ing.fernandovg@gmail.com>
2024-08-02 14:56:30 -0600

    Give albumId to Song model

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,12 @@
 const Album = require("./Album");
 const Artist = require("./Artist");
+const Song = require("./Song");
 
-
+    //! Album --> ArtistID
 Album.belongsTo(Artist);
 Artist.hasMany(Album);
+
+// Song --> AlbumID
+Song.belongsTo(Album);
+Album.hasMany(Song);
 
